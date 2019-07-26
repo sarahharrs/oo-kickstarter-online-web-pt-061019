3bb331b9b4d409d18d6d46b9740fc6dc0b1855a1
Sarah Harrs <sarahharrs@gmail.com>
2019-07-26 17:20:10 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,14 @@
 'require pry'
 
 class Project
-  attr_accessor :title, :backers
+  attr_accessor :title, :backed_projects
 
   def initialize(title)
     @title = title
-    @backers = []
+    @backed_projects = []
   end
 
-  def add_backer(backer)
-    @backers << backer
+  def back_project(project)
+    backed_projects << project
     backer.backed_projects << self
   end
